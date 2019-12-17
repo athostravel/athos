6b97440878ec0cd5a010d99012c45f06736228de
Jorge Raboso Barceló <>
2019-12-17 11:06:37 +0100

    Button Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as HelloWorld } from '@components/HelloWorld'
 export { default as ByeWorld } from '@components/ByeWorld'
+export { default as Button } from '@components/Button'
 export { default as Picture } from '@components/Picture'
+export { default as Price } from '@components/Price'
+export { default as PriceButton } from '@components/Price/PriceButton'
