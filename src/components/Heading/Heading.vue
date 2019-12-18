5e7df17740ef2511efa069550cdac7c6afbcfc3a
Jorge Raboso Barceló <>
2019-12-18 08:18:50 +0100

    heading component

--- src/components/Heading/Heading.vue
+++ src/components/Heading/Heading.vue
@@ -11,17 +11,17 @@
             :is="titleTag"
             class="c-heading__title"
         >
-            <span class="c-heading__icon" v-if="icon">
+            <span v-if="icon" class="c-heading__icon">
                 <AtIcon :icon="icon" />
             </span>
             {{ title }}
         </component>
         <p
-            :key="txt.id"
-            class="c-heading__paragraph"
             v-for="txt in text"
+            :key="txt.id"
+            class="c-heading__par"
         >
-            <span class="c-heading__paragraph-text">
+            <span class="c-heading__par-text">
                 {{ txt }}
             </span>
         </p>
@@ -32,12 +32,14 @@
   :root {
     --c-heading-icon-padding-right: #{em(8px)};
     --c-heading-padding-bottom: #{em(32px)};
-    --c-heading-paragraph-color: currentColor;
-    --c-heading-paragraph-margin-bottom: #{em(8px)};
-    --c-heading-paragraph-margin-top: #{em(16px)};
     --c-heading-title-font-size: #{em(32px)};
     --c-heading-title-line-height: 1.4;
     --c-heading-title-color: var(--color-primary);
+    --c-heading-par-font-size: #{em(16px)};
+    --c-heading-par-line-height: 1.4;
+    --c-heading-par-color: currentColor;
+    --c-heading-par-margin-bottom: #{em(8px)};
+    --c-heading-par-margin-top: #{em(16px)};
   }
 </style>
 
@@ -70,15 +72,17 @@
       display: flex;
     }
 
-    &__paragraph {
-      color: var(--c-heading-paragraph-color);
+    &__par {
+      color: var(--c-heading-par-color);
+      font-size: var(--c-heading-par-font-size);
+      line-height: var(--c-heading-par-line-height);
 
       &:not(:last-of-type) {
-        margin-bottom: var(--c-heading-paragraph-margin-bottom);
+        margin-bottom: var(--c-heading-par-margin-bottom);
       }
 
       &:first-of-type {
-        margin-top: var(--c-heading-paragraph-margin-top);
+        margin-top: var(--c-heading-par-margin-top);
       }
     }
   }
