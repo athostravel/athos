d4578969bf52a61eb9cd4b40f946451dfa74ddc7
Javi Puche <javierpuchevictoria@gmail.com>
2020-01-21 08:51:15 +0100

    remove input

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -133,7 +133,6 @@ module.exports = {
                                 '/es/documentacion/components/button',
                                 '/es/documentacion/components/checkbox',
                                 '/es/documentacion/components/icon',
-                                '/es/documentacion/components/input',
                                 '/es/documentacion/components/picture',
                                 '/es/documentacion/components/price-button',
                                 '/es/documentacion/components/price',
