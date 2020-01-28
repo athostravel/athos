fdc0ea85fb4720186867defa99b5952dbfc34e5e
Javi Puche <javierpuchevictoria@gmail.com>
2020-01-28 11:32:13 +0100

    Added heading component

---
+++ examples/components/Heading/Heading.vue
@@ -0,0 +1,16 @@
+<template>
+    <AtHeading v-bind="example" />
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                example: {
+                    title: 'Las mejores ofertas en hoteles, vuelos, paquetes y vacaciones',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis tempora mollitia accusantium assumenda quibusdam temporibus molestiae dicta corporis quos reprehenderit'
+                }
+            }
+        }
+    }
+</script>
--- src/pages/PageHydra/PageHydra.vue
+++ src/pages/PageHydra/PageHydra.vue
@@ -1,5 +1,7 @@
 <template>
     <AtLayoutVirgo>
+        <AtHeading v-if="heading" v-bind="[{ config: cfg.heading }, heading]" />
+
         <AtSectionLynx
             v-if="caribbeanOffers && caribbeanOffers.items"
             v-bind="[{ config: cfg.caribbeanOffers }, caribbeanOffers]"
@@ -9,18 +11,32 @@
 
 <script>
     import AtLayoutVirgo from '@layouts/LayoutVirgo/LayoutVirgo'
+    import AtHeading from '@components/Heading/Heading'
     import AtSectionLynx from '@components/SectionLynx/SectionLynx'
 
     export default {
         name: 'AtPageHydra',
         components: {
+            AtHeading,
             AtLayoutVirgo,
             AtSectionLynx
         },
         props: {
+            heading: {
+                type: Object,
+                default: () => {}
+            },
             caribbeanOffers: {
                 type: Object,
                 default: () => {}
+            }
+        },
+        data () {
+            return {
+                cfg: {
+                    heading: {},
+                    caribbeanOffers: {}
+                }
             }
         }
     }
